24d50aa26cb889a8630987d8d3f68201c322bf9e
Saman Pandey <singularity.1@hotmail.com>
2026-07-18 19:08:59 +0530

    fix: add missing conf for VPS deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["app.local.singularitydev.xyz", "http://localhost:3000", "http://localhost:3001"],
 };
 
 export default nextConfig;
